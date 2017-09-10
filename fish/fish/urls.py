81d94c28b85ca3a4d3caadf22aa8f3c21b1fef83
TiGRoN <tigron.dev@gmail.com>
2017-09-10 20:07:22 +0300

    Managing static files right

--- fish/fish/urls.py
+++ fish/fish/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from main import urls as main_urls
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 	url(r'^admin/$', include(admin.site.urls)),
 	url(r'', include(main_urls))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
